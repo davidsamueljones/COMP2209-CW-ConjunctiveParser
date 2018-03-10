7723027154067ccda411d080d5fe57ace74a7e2b
David Jones <davidsamueljones@icloud.com>
2018-03-10 22:45:42 +0000

    Added table import code
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,9 +1,61 @@
 module Interpreter where
 import Grammar
+import Control.Exception
+import Text.Regex (splitRegex, mkRegex)
+import Data.Either.Compat
+import Control.Monad
+import System.Exit   
 
 -- TODO: Just ya know, write this thing?
 runInterpreter :: Show a => Prog -> [a]
 runInterpreter ast = []
+
+
+-- Imports a csv file into zipped columns
+importTable :: FilePath -> IO (Either InterException [[String]])
+importTable f = do 
+    res <- try $ readFile f :: IO (Either IOError String)
+    case res of
+      Left e -> throw (InterExceptionImport e)
+      Right dat -> do
+        -- FIXME: Does not allow commas in strings, can't use lookbehind due to POSIX regex -- (?<!\\)
+        let tokenise = map (splitRegex (mkRegex ",")) . lines
+        return (multiZip' $ tokenise dat)
+
+-- Zip rows into column lists, failing if columns are not all equal length
+multiZip' :: [[a]] -> Either InterException [[a]]
+multiZip' xss | allLensSame xss = Right (multiZip xss)
+multiZip' xss | otherwise       = throw InterExceptionZip
+
+-- Zip rows into column lists
+multiZip :: [[a]] -> [[a]]
+multiZip xss | maxListLen xss == 0 = []
+multiZip xss = foldl (++) [] line : multiZip rest
+    where line = map (take 1) xss
+          rest = map (drop 1) xss
+
+-- Find the longest row in a list
+maxListLen :: [[a]] -> Int
+maxListLen xss = maximum $ map length xss
+
+-- Get length of all rows, true if all equal
+allLensSame :: [[a]] -> Bool
+allLensSame xss = allValsSame $ map length xss
+
+-- True if all values in list are equal 
+allValsSame :: Eq a =>[a] -> Bool
+allValsSame xs = all (== head xs) (tail xs)
+
+
+-----------------------------------------------------------------
+-- Interpreter Exceptions
+-----------------------------------------------------------------
+data InterException = InterExceptionZip
+                    | InterExceptionImport IOError
+                    deriving (Show)
+
+instance Exception InterException
+
 
 
 -- Our Rules:
