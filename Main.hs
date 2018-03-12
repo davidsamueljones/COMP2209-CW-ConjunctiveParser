69a66757cf8c9aee5b96ef7773d610f37068f6be
David Jones <davidsamueljones@icloud.com>
2018-03-12 22:13:42 +0000

    Updated main to work from command line
    Currently opens default file vs shows error on no arguments
    
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- Main.hs
+++ Main.hs
@@ -3,26 +3,44 @@ import Tokens
 import Grammar
 import Interpreter
 
+import System.Environment
 import Control.Exception
 import Control.Monad
 import System.Exit   
 
-
 main :: IO ()
-main =   do 
-  dat <- readFile "test_files/test3.txt"
-  -- Lex input into tokens
-  let lexerResult = lexString dat
-  exitIfError lexerResult
-  -- No lex error so use tokens as input for parser
-  let tokens = Main.fromRight [] lexerResult
-  parseResult <- runParse tokens
-  exitIfError parseResult
-  -- No parse error so use AST for interpretation 
-  let ast = Main.fromRight (Prog [] []) parseResult
-  --let output = runInterpreter ast
-  --putStrLn output -- FIXME: Do interpretation
-  putStrLn $ show ast
+main = do args <- getArgs
+          case args of
+            [f] -> interpretFile f
+            _      -> interpretFile "p1.cql"
+            -- _      -> do putStrLn "\nInvalid input arguments, correct usage is:\n\t myinterpreter <input>\n"
+            --              exitFailure
+
+interpretFile :: FilePath -> IO ()
+interpretFile f = readFile f >>= interpretString
+
+interpretString :: String -> IO () 
+interpretString dat = do
+    -- Lex input into tokens
+    let lexerResult = lexString dat
+    exitIfError lexerResult
+    -- No lex error so use tokens as input for parser
+    let tokens = Main.fromRight [] lexerResult
+    --putStrLn $ show tokens
+    parseResult <- runParse tokens
+    exitIfError parseResult
+    -- No parse error so use AST for interpretation 
+    let ast = Main.fromRight (Prog [] []) parseResult
+    --putStrLn $ show ast
+    interResult <- runInterpreter ast
+    exitIfError interResult
+    -- No interpret error
+    let output = Main.fromRight "" interResult
+    putStrLn output
+    -- output <- runInterpreter ast
+    -- putStrLn output -- FIXME: Do interpretation
+
+
 
 
 exitIfError :: Show a => Either a b -> IO ()
