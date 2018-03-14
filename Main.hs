787b8a99532654ae8f6976ddd6de719e19fa3432
hce1g16 <36552418+hce1g16@users.noreply.github.com>
2018-03-14 13:39:45 +0000

    Added problems.txt

--- Main.hs
+++ Main.hs
@@ -14,7 +14,7 @@ main :: IO ()
 main = do args <- getArgs
           case args of
             [f] -> interpretFile f
-            _      -> interpretFile "p1.cql"
+            _      -> interpretFile "p3.cql"
             -- _      -> do putStrLn "\nInvalid input arguments, correct usage is:\n\t myinterpreter <input>\n"
             --              exitFailure
 
